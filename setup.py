9a802d7cbd8fc68e8c3c59b9a95fb48c297bb741
Thomas Berdy <thomas.berdy@outlook.com>
2018-08-13 00:45:51 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='encoding_tools',
-    version="0.0.1",
+    version="0.0.2",
     description="A package to deal with encoding.",
     long_description=long_description,
     long_description_content_type='text/markdown',
